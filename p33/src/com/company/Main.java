7d00f593f4284b83965ea491e9a128a17ecd4d6a
Shevikina <Shevik.vlog@gmail.com>
2021-10-03 15:12:20 +0300

    В этой задаче тебе предстоит реализовать метод getPrediction() в классе MagicBall, который будет работать как шар предсказаний. Он случайным образом возвращает фразу-ответ на заданный вопрос. В методе getPrediction() нужно использовать метод nextInt(int) класса Random, который должен возвращать значения от 0 до 7 включительно. В зависимости от результата работы метода nextInt(int) возвращается один из вариантов ответа: 0 -> Бесспорно 1 -> Определённо да 2 -> Вероятнее всего 3 -> Хорошие перспективы 4 -> Спроси позже 5 -> Попробуй снова 6 -> Мой ответ — нет 7 -> Весьма сомнительно Иначе вернуть null.

---
+++ p33/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Заработаю ли я себе на BMW?");
+        System.out.println(MagicBall.getPrediction());
+    }
+}
