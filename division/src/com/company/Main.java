01f3ae553e804dc359d1a383cf4afa90d9f6949c
Shevikina <Shevik.vlog@gmail.com>
2021-11-22 17:15:41 +0300

    Количество двузначных чисел, кратных step, но не кратных 10 и не содержащих одинаковых цифр в себе.

--- division/src/com/company/Main.java
+++ division/src/com/company/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	int amount=0;
-    int step=1;
-    for(int i=10;i<100;i+=step)
-        amount++;
+    int step=18;
+    for(int i=10;i<100;i++)
+        if(i%step==0&&(i%10!=(i%100)-i%10)&&i%10!=0) amount++;
     System.out.println(amount);
     }
 }
