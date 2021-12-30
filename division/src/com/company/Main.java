aa429dc995f10d372435c4a066f056df22b0ae58
Shevikina <Shevik.vlog@gmail.com>
2021-12-30 13:16:56 +0300

    изменения

--- division/src/com/company/Main.java
+++ division/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	int amount=0;
-    int step=18;
+    int step=42;
     for(int i=10;i<100;i++)
         if(i%step==0&&(i%10!=(i%100)-i%10)&&i%10!=0) {
             System.out.println(i);
